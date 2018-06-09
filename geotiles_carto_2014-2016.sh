b45cf0793f29dd821081dd72ea64d6e0f2def235
Daniel Trone <danieljtrone@gmail.com>
2018-06-09 11:07:58 +0000

    no mbtiles output.  fix extra directory created

--- geotiles_carto_2014-2016.sh
+++ geotiles_carto_2014-2016.sh
@@ -15,8 +15,8 @@ npm install -g mapshaper
 declare -a state_fips=('01' '02' '04' '05' '06' '08' '09' '10' '11' '12' '13' '15' '16' '17' '18' '19' '20' '21' '22' '23' '24' '25' '26' '27' '28' '29' '30' '31' '32' '33' '34' '35' '36' '37' '38' '39' '40' '41' '42' '44' '45' '46' '47' '48' '49' '50' '51' '53' '54' '55' '56' '60' '66' '69' '72' '78');
 
 # clean old (just in case) and create temporary directories
-rm -rf ./downloads ./geojson ./tiles ./cluster-tiles ./unzipped ./merged-geojson ./aggregated-geojson ./tiled-aggregated ./aggregated-geojson
-mkdir ./downloads ./geojson ./tiles ./cluster-tiles ./unzipped ./merged-geojson ./aggregated-geojson ./tiled-aggregated ./aggregated-geojson ./mbtiles
+rm -rf ./downloads ./geojson ./tiles ./cluster-tiles ./unzipped ./merged-geojson ./aggregated-geojson ./tiled-aggregated
+mkdir ./downloads ./geojson ./tiles ./cluster-tiles ./unzipped ./merged-geojson ./aggregated-geojson ./tiled-aggregated
 
 numberargs=$#
 
@@ -44,7 +44,6 @@ then
     
     # create county or state tiles
     tippecanoe -e ./tiles/"$geolayer"_"$year" -l main -pt -ab -z9 -Z3 -y GEOID -y NAME ./geojson/cb_"$year"_us_"$geolayer"_500k.geojson
-    tippecanoe -e ./mbtiles/"$geolayer"_"$year".mbtiles -l main -pt -ab -z9 -Z3 -y GEOID -y NAME ./geojson/cb_"$year"_us_"$geolayer"_500k.geojson
 fi
 
 if [ "$geolayer" == "place" ] || [ "$geolayer" == "tract" ] || [ "$geolayer" == "bg" ] ;
@@ -84,7 +83,6 @@ then
     
     # join all individual zoom level tiles together
     tile-join -e ./tiles/"$geolayer"_"$year" ./tiled-aggregated/"$geolayer"_"$year"_*.mbtiles
-    tile-join -o ./mbtiles/"$geolayer"_"$year".mbtiles ./tiled-aggregated/"$geolayer"_"$year"_*.mbtiles
 fi
 
     # Upload directory to s3
@@ -94,4 +92,4 @@ fi
 
     
 # clean up
-rm -rf ./downloads ./geojson ./tiles ./cluster-tiles ./unzipped ./merged-geojson ./aggregated-geojson ./tiled-aggregated ./aggregated-geojson
+rm -rf ./downloads ./geojson ./tiles ./cluster-tiles ./unzipped ./merged-geojson ./aggregated-geojson ./tiled-aggregated
